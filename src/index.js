143a2413db705bf4a658984e1d967f91bb4e840f
黃哲芬 <annie101101046@gmail.com>
2019-04-08 12:17:48 +0800

    webpagedone

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 alert("Hello Webpack");
-import React from "react";
 import ReactDOM from "react-dom";
+import React from "react";
+
+
 
 //react JSX 寫法
 class MyHead extends React.Component {
+
     constructor(props) {
         super(props);
     }
+
     render() {
         return <h3 > Hello React JSX < /h3>; 
     }
