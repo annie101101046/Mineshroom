4397e6797801691a725b0c2172118d36674db781
黃哲芬 <annie101101046@gmail.com>
2019-04-27 23:52:36 +0800

    done

--- shroomMall.js
+++ shroomMall.js
@@ -342,7 +342,7 @@
             console.log(web3);
             //在這裡以下做事才有用ㄚ web3 連到了
             let myContract = new web3.eth.Contract(abi,
-                "0x867d88ccc6faf2a7b7982b2b1a349164c6ac4dce");
+                "0x80efe9bfaaa22aa86f52de3ceea3225bfcf4fb47");
             console.log(myContract);
             myContract.methods.isRegister(window.dexon.defaultAccount).call().then(e => {
                 console.log(e);
@@ -361,7 +361,7 @@
                     v.innerHTML = balance + 'DXN';
 
                     let myContract = new web3.eth.Contract(abi,
-                        "0x867d88ccc6faf2a7b7982b2b1a349164c6ac4dce");
+                        "0x80efe9bfaaa22aa86f52de3ceea3225bfcf4fb47");
                     console.log(myContract);
                     //拿菇幣數量
                     myContract.methods.getGuCoin(window.dexon.defaultAccount).call()
